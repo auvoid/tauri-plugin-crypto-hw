a915dbe5533bc68a72c1b8e5cf1458c361681e33
Soham Jaiswal <sosweetham@gmail.com>
2025-05-05 09:13:49 +0530

    feat: setup workspace

---
+++ src/desktop.rs
@@ -0,0 +1,22 @@
+use serde::de::DeserializeOwned;
+use tauri::{plugin::PluginApi, AppHandle, Runtime};
+
+use crate::models::*;
+
+pub fn init<R: Runtime, C: DeserializeOwned>(
+  app: &AppHandle<R>,
+  _api: PluginApi<R, C>,
+) -> crate::Result<Crypto<R>> {
+  Ok(Crypto(app.clone()))
+}
+
+/// Access to the crypto APIs.
+pub struct Crypto<R: Runtime>(AppHandle<R>);
+
+impl<R: Runtime> Crypto<R> {
+  pub fn ping(&self, payload: PingRequest) -> crate::Result<PingResponse> {
+    Ok(PingResponse {
+      value: payload.value,
+    })
+  }
+}
